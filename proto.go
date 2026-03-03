944340611ddaabc5e6dc35b94091edf5299c174c
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-03-03 13:08:47 +0330

    feat: implement RESP for the peers

---
+++ proto.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/tidwall/resp"
+)
+
+// we need to implement redis serializtion protocol which clients use to communicate with the redis server.
+
+type Command interface {
+}
+
+type SetCommand struct {
+	key, val string
+}
+
+func parseCommand(raw string) (Command, error) {
+
+	rd := resp.NewReader(bytes.NewBufferString(raw))
+
+	for {
+		v, _, err := rd.ReadValue()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Read %s\n", v.Type())
+		if v.Type() == resp.Array {
+			for i, v := range v.Array() {
+				fmt.Printf("  #%d %s, value: '%s'\n", i, v.Type(), v)
+			}
+		}
+	}
+
+	return "foo", nil
+}
